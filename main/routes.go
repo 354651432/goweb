080f0111157b09f0507c8a871147a3208052e701
shizhuo <shizhuo@shhuzhong.com>
2019-08-16 18:56:19 +0800

    modify readme comments

--- main/routes.go
+++ main/routes.go
@@ -3,20 +3,22 @@ package main
 import (
 	"app1/router"
 	"fmt"
-	"io"
 	"net/http"
 )
 
 func registerRoutes() {
 
+	// access /
 	router.Redirect("/", "/home")
 
+	// access /home
 	router.Get("/home", func(res http.ResponseWriter, req *http.Request) {
-		_, _ = io.WriteString(res, "<h1>it works</h1?")
+		router.WriteContent(res, "<h1>it works</h1?")
 	})
 
+	// access /about
 	router.Get("/about", func(res http.ResponseWriter, req *http.Request) {
-		_, _ = io.WriteString(res, "<h1>go router code by dual@dr.h</h1?")
+		router.WriteContent(res, "<h1>go router code by dual@dr.h</h1?")
 	})
 
 	router.Get("/users", func(res http.ResponseWriter, req *http.Request) {
@@ -34,6 +36,7 @@ func registerRoutes() {
 		})
 	})
 
+	// access /users/2
 	router.Get("/users/@id:[0-9]+", func(res http.ResponseWriter, req *http.Request) {
 		id := router.GetParams("id")
 		router.WriteJson(res, map[string]interface{}{
@@ -43,10 +46,12 @@ func registerRoutes() {
 		})
 	})
 
+	// access /users/create
 	router.Get("/users/create", func(res http.ResponseWriter, req *http.Request) {
-		_, _ = io.WriteString(res, "<h1>create user page</h1?")
+		router.WriteContent(res, "<h1>create user page</h1?")
 	})
 
+	// access /users/2/scores/3
 	router.Get("/users/@user_id:[0-9]+/scores/@score_id", func(res http.ResponseWriter, req *http.Request) {
 		userId := router.GetParams("user_id")
 		scoreId := router.GetParams("score_id")
